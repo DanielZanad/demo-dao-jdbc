cb2782d7126ca60ed50fd25ad5891d4f8fdf1ea1
DanielZanad <daniel.zanad.dz@gmail.com>
2022-11-02 15:55:29 -0300

    DeleteById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,11 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Program {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== Test 1: Seller findById ===");
@@ -50,6 +51,15 @@ public class Program {
         seller.setEmail("gus@fring.com");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n=== Test 6: Seller delete ===");
+        System.out.print("Enter with id for delete test: ");
+        int id = sc.nextInt();
+
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
 
 
     }
